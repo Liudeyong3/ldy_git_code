4ce02ca3422ea147e76fe8002ecf4bb7aed157ac
devin <dpn2010013@163.com>
2023-08-06 13:27:14 +0800

    this is the first commit test

---
+++ 1_git_first.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @FileName  :1_git_first.py
+# @Time      :2023/8/6 11:47
+# @Author    :Deyong Liu
+
+
+def main():
+    print("this is the first git project")
+
+
+if __name__ == "__main__":
+    main()
+
